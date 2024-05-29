6e11fb6a7e0abca4dce4a1eda7e340e96be1b747
Lucas Bonomo <bonomo.lucas@gmail.com>
2024-05-29 09:37:01 -0300

    Cleaning gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,7 @@
-/*
-# Debug
+/**
+ * Grunt file.
+ */
 
-
-# Dist
-*.php    = Sincronizar
-*.css    = en "dst"
-*.js     = en "dst"
-*.scss   = transcompilar style.scss > CSS en "dst"
-imagenes = optimizar y copiar en "dst"
-
-https://github.com/gruntjs/grunt-contrib-watch
-
-
-*/
 module.exports = function (grunt) {
   const extensions = "{css,php,js,html,png,svg,txt}";
 
@@ -23,50 +12,14 @@ module.exports = function (grunt) {
           src: [`theme/dst/**/*.${extensions}`],
         },
         options: {
-          // server: { baseDir: "./" }
           watchTask: true,
           open: false,
-          proxy: "https://uou.lndo.site/",
+          proxy: "https://lucasbonomo.lndo.site/",
         },
       },
     },
 
-    // uglify: {
-    //   dist: {
-    //     files: {
-    //       "./theme/dst/assets/js/build.js": ["theme/src/assets/js/*.js"],
-    //     },
-    //   },
-    // },
-
     sync: {
-      // acf_json: {
-      //   files: [
-      //     {
-      //       cwd: "theme/src/acf-json/",
-      //       src: ["*.json"],
-      //       dest: "theme/dst/acf-json/",
-      //     },
-      //   ],
-      // },
-      // json_import: {
-      //   files: [
-      //     {
-      //       cwd: "theme/dst/acf-json/",
-      //       src: ["*.json"],
-      //       dest: "theme/src/acf-json/",
-      //     },
-      //   ],
-      // },
-      // plugins: {
-      //   files: [
-      //     {
-      //       cwd: "plugins/honeycomb-blocks/src/",
-      //       src: [`**/*`],
-      //       dest: "plugins/honeycomb-blocks/dst/",
-      //     },
-      //   ],
-      // },
       theme: {
         files: [
           {
@@ -76,10 +29,8 @@ module.exports = function (grunt) {
           },
         ],
       },
-      verbose: true, // Display log messages when copying files
-      // pretend: true, // Don't do any IO. Before you run the task with `updateAndDelete` PLEASE MAKE SURE it doesn't remove too much.
-      updateAndDelete: true, // Remove all files from dest that are not found in src. Default: false
-      // compareUsing: "md5" // compares via md5 hash of file contents, instead of file modification ti
+      verbose: true,
+      updateAndDelete: true,
     },
 
     watch: {
